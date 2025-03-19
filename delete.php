d1e60ae33afc82879c6c0e295bc5c71c1bf730c0
Vladimir Gromov <vovamakukha@gmail.com>
2025-03-19 16:15:04 -0300

    Update

--- delete.php
+++ delete.php
@@ -3,9 +3,7 @@
 if (isset($_POST['DELETE'])) {
     
     $input = $_SERVER['REQUEST_URI'];
-    var_dump($input);
     $id = preg_replace("/[^,.0-9]/", '', $input);
-    var_dump($id);
-    // $s = $conn->query("DELETE FROM datatable WHERE id =$id");
-    // $all = $s->fetch(PDO::FETCH_ASSOC);
+    $s = $conn->query("DELETE FROM datatable WHERE id =$id");
+    $all = $s->fetch(PDO::FETCH_ASSOC);
 }
